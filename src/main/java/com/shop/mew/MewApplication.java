0d65816a0395cfda117e748b56a46e889842bdb5
syh <dbsgur6896@naver.com>
2021-02-26 12:16:25 +0900

    프로젝트 생성

---
+++ src/main/java/com/shop/mew/MewApplication.java
@@ -0,0 +1,13 @@
+package com.shop.mew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shop/mew/MewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.mew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
