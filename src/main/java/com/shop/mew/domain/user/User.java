cad136bead7cdf693c1b2095e04474a0e33fa322
syh <dbsgur6896@naver.com>
2021-02-26 12:17:07 +0900

    User 생성

---
+++ src/main/java/com/shop/mew/domain/user/User.java
@@ -0,0 +1,49 @@
+package com.shop.mew.domain.user;
+
+import com.shop.mew.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private LocalDate birth;
+
+    private String address;
+
+    private boolean agreeMessageByEmail;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Builder
+    public User(Long id, String name, String email,
+                String password, LocalDate birth,
+                String address, boolean agreeMessageByEmail,
+                Role role) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.birth = birth;
+        this.address = address;
+        this.agreeMessageByEmail = agreeMessageByEmail;
+        this.role = role;
+    }
+}
