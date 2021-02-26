4a268f7ef3e54931d4858710790e1bdbb572cbdc
syh <dbsgur6896@naver.com>
2021-02-26 16:22:37 +0900

    User 생성, 조회 controller

---
+++ src/main/java/com/shop/mew/web/ItemController.java
@@ -0,0 +1,9 @@
+package com.shop.mew.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class ItemController {
+}
---
+++ src/main/java/com/shop/mew/web/UserController.java
@@ -0,0 +1,31 @@
+package com.shop.mew.web;
+
+import com.shop.mew.service.UserService;
+import com.shop.mew.web.dto.UserRequestDto;
+import com.shop.mew.web.dto.UserResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+public class UserController {
+
+    private final UserService userService;
+
+    //회원 생성
+    @PostMapping ("/user")
+    public ResponseEntity<?> addUser(@Valid @RequestBody UserRequestDto.Join userRequestDto) {
+        userService.add(userRequestDto);
+        return ResponseEntity.ok("회원 가입 성공");
+    }
+
+    //회원 프로필 조회
+    @GetMapping("/user/{id}")
+    public UserResponseDto.Profile getProfile(@PathVariable("id") Long id){
+        return userService.getProfile(id);
+    }
+}
