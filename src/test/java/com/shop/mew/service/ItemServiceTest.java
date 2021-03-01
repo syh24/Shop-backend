f95a50fc3d8c7ecbf12c1a769d2b25527d2ce869
syh <dbsgur6896@naver.com>
2021-03-01 21:57:20 +0900

    상품 조회, 수정 테스트 완료

--- src/test/java/com/shop/mew/service/ItemServiceTest.java
+++ src/test/java/com/shop/mew/service/ItemServiceTest.java
@@ -1,28 +1,27 @@
 package com.shop.mew.service;
 
+import com.shop.mew.domain.item.Item;
 import com.shop.mew.web.dto.ItemRequestDto;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.annotation.Commit;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
+@Rollback(value = false)
 class ItemServiceTest {
 
     @Autowired
     private ItemService itemService;
 
     @Test
-    @Commit
     void 상품추가 () throws Exception {
         ItemRequestDto.Register item = ItemRequestDto.Register.builder()
                 .name("test")
@@ -32,13 +31,10 @@ class ItemServiceTest {
                 .price(10000)
                 .build();
 
-        System.out.println("item = " + item);
-
         itemService.addItem(item);
-
-        assertThat(itemService.findAll().get(0).getName()).isEqualTo("test");
-
+        List<Item> result = itemService.findAll();
+        for (Item item1 : result) {
+            System.out.println("item1 = " + item1.getPrice());
+        }
     }
-
-
 }
