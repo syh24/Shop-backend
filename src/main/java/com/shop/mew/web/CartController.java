1414f159f89ad85694eee96b83fe88b77df9a24d
syh <dbsgur6896@naver.com>
2021-03-19 19:10:35 +0900

    CartController 수정

--- src/main/java/com/shop/mew/web/CartController.java
+++ src/main/java/com/shop/mew/web/CartController.java
@@ -1,26 +1,16 @@
 package com.shop.mew.web;
 
-import com.shop.mew.domain.cart.Cart;
 import com.shop.mew.service.CartService;
 import com.shop.mew.web.dto.CartRequestDto;
-import com.shop.mew.web.dto.CartResponseDto;
-import com.shop.mew.web.dto.ItemResponseDto;
-import com.shop.mew.web.dto.UserResponseDto;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.HashMap;
-import java.util.List;
-import java.util.UUID;
-import java.util.stream.Collectors;
-
-import static org.springframework.http.ResponseEntity.ok;
 
 @Api(tags = "찜 목록 APIs")
 @RequestMapping("/api/v1")
@@ -38,7 +28,7 @@ public class CartController {
         } catch (Exception e) {
             return new ResponseEntity<>("잘못된 값입니다.", HttpStatus.BAD_REQUEST);
         }
-        return ResponseEntity.ok().body("장바구니에 추가되었습니다.");
+        return ResponseEntity.ok("장바구니에 추가되었습니다.");
     }
 
     @ApiOperation(value = "찜 목록 조회")
