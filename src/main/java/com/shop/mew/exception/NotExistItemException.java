594fae98576f3bcf50ff79dfe667d28864876c14
syh <dbsgur6896@naver.com>
2021-03-20 14:22:45 +0900

    예외처리 추가

--- src/main/java/com/shop/mew/exception/NotExistItemException.java
+++ src/main/java/com/shop/mew/exception/NotExistItemException.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.FORBIDDEN)
-public class NotExistItenException extends RuntimeException{
-    public NotExistItenException(String message) {
+public class NotExistItemException extends RuntimeException{
+    public NotExistItemException(String message) {
         super(message);
     }
 }
