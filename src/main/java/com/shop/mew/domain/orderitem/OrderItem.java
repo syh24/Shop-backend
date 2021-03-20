82c12fce2eaa5e6e4d404302df89d4edb6bb492b
syh <dbsgur6896@naver.com>
2021-03-20 16:07:17 +0900

    주문 내역 상세, 조회 추가

--- src/main/java/com/shop/mew/domain/orderitem/OrderItem.java
+++ src/main/java/com/shop/mew/domain/orderitem/OrderItem.java
@@ -1,13 +1,17 @@
 package com.shop.mew.domain.orderitem;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.shop.mew.domain.BaseTimeEntity;
 import com.shop.mew.domain.cart.Cart;
 import com.shop.mew.domain.enums.OrderStatus;
 import com.shop.mew.domain.user.User;
+import com.shop.mew.web.dto.OrderItemResponseDto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
@@ -32,9 +36,26 @@ public class OrderItem extends BaseTimeEntity {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
+    @JsonIgnore
     private User user;
 
     @OneToMany(mappedBy = "orderItem")
+    @JsonIgnore
     private List<Cart> carts;
 
+    public OrderItemResponseDto toResponseDto() {
+        LocalDateTime createdDate = this.getModifiedDate();
+        return OrderItemResponseDto.builder()
+                .id(id)
+                .orderNumber(orderNumber)
+                .orderName(orderName)
+                .deliveryMessage(message)
+                .orderStatus(orderStatus)
+                .amount(amount)
+                .createdDate(createdDate.getYear() + "." + createdDate.getMonthValue() + "."
+                        + createdDate.getDayOfMonth() + " " + createdDate.getHour() + ":" + createdDate.getMinute() + ":"
+                        + createdDate.getSecond())
+                .carts(carts)
+                .build();
+    }
 }
--- src/main/java/com/shop/mew/domain/orderitem/OrderItemRepository.java
+++ src/main/java/com/shop/mew/domain/orderitem/OrderItemRepository.java
@@ -2,5 +2,9 @@ package com.shop.mew.domain.orderitem;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+    List<OrderItem> findAllByUserId(Long userId);
 }
