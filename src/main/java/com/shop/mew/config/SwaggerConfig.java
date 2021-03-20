ef8719c6b4ebe4be0f18a9c9c019371375a61b3e
syh <dbsgur6896@naver.com>
2021-03-20 14:23:46 +0900

    권한 수정

--- src/main/java/com/shop/mew/config/SwaggerConfig.java
+++ src/main/java/com/shop/mew/config/SwaggerConfig.java
@@ -1,23 +1,57 @@
 package com.shop.mew.config;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.nio.charset.StandardCharsets;
+
+import static java.util.Collections.singleton;
+import static springfox.documentation.builders.RequestHandlerSelectors.*;
+
+
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+public class SwaggerConfig implements WebMvcConfigurer {
+
+    private ApiInfo swaggerInfo(){
+        return new ApiInfoBuilder()
+                .title("Mew API Documentation")
+                .contact(new Contact("syh",null,"dbsgur6896@naver.com"))
+                .version("1.0.0")
+                .build();
+    }
 
     @Bean
-    public Docket api() {
+    public Docket api (){
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(swaggerInfo())
+                .produces(singleton("application/json"))
+                .consumes(singleton("application/json"))
+                .useDefaultResponseMessages(false)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(withMethodAnnotation(ApiOperation.class))
                 .paths(PathSelectors.any())
                 .build();
     }
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
 }
+
--- src/main/java/com/shop/mew/config/auth/SecurityConfig.java
+++ src/main/java/com/shop/mew/config/auth/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.shop.mew.domain.user.Role;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,19 +15,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-            http
+        http
                 .csrf().disable()
                 .headers().frameOptions().disable()
                 .and()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/", "/css/**",
-                        "/images/**", "/js/**", "/h2-console/**", "/api/v1/category").permitAll()
-                .antMatchers(HttpMethod.POST, "/api/v1/user", "/api/v1/authentication").permitAll()
-                .antMatchers("/api/v1/profile/**", "/api/v1/user/**").permitAll()//hasAnyRole(Role.ADMIN.name(), Role.USER.name())
-                .anyRequest().permitAll()
+                        "/images/**", "/js/**", "/h2-console/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/v?/user/**").permitAll()
+                .antMatchers( HttpMethod.GET,"/api/v?/user").hasRole(Role.ADMIN.name())
+                .antMatchers(HttpMethod.POST,"/api/v?/item/add").hasRole(Role.ADMIN.name())
+                .antMatchers(HttpMethod.PUT,"/api/v?/item/**").hasRole(Role.ADMIN.name())
+                .antMatchers(HttpMethod.DELETE,"/api/v?/item/**").hasRole(Role.ADMIN.name())
+                .anyRequest().authenticated()
                 .and()
-                .logout()
-                .logoutSuccessUrl("/");
+                .formLogin().disable();
+    }
+
+    @Override
+    public void configure (WebSecurity web) throws Exception{
+        web
+                .ignoring()
+                .antMatchers("/h2-console/**",
+                        "/favicon.ico", "/swagger-ui.html/**", "/swagger-resources/**",
+                        "/v?/api-docs", "/webjars/**", "/static/**");
     }
 
     @Bean
